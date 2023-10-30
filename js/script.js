c72dc0cd084f3d875bbd54fb03d4e03f8ad125cd
CamiloGonzalezPineda <camiloandresgonzalezpineda@MacBook-Pro-de-Camilo.local>
2023-10-30 13:03:52 -0500

    actualizado

--- js/script.js
+++ js/script.js
@@ -17,3 +17,37 @@ function validacion() {
     alert("welcome");
     
 }
+//-------------
+/*
+let carrusel = document.getElementById('carrusel')
+let camisetas = document.getElementById('camisetas')
+let pantalones = document.getElementById('pantalones')
+let buzos = document.getElementById('buzos')
+let zapatos = document.getElementById('zapatos')
+*/
+function mostarCamisetas(){
+   location.href = './views/camisetas.html'
+}
+function mostrarPantalones(){
+    camisetas.style.display ='none'
+    carrusel.style.display = 'none'
+    pantalones.style.display = 'block'
+    buzos.style.display = 'none'
+    zapatos.style.display = 'none'
+}
+function mostrarBuzos(){
+    camisetas.style.display ='none'
+    carrusel.style.display = 'none'
+    pantalones.style.display = 'none'
+    buzos.style.display = 'block'
+    zapatos.style.display = 'none'
+}
+function mostrarZapatos(){
+    camisetas.style.display ='none'
+    carrusel.style.display = 'none'
+    pantalones.style.display = 'none'
+    buzos.style.display = 'none'
+    zapatos.style.display = 'block'
+}
+
+//________________________________________________________________
